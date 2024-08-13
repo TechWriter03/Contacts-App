e111b11fb693b43829823a54f1ead578b2802bc3
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-08-13 20:30:07 +0530

    Update ContactCard.js

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -4,20 +4,20 @@ import user from '../images/user-logo.png';
 
 const ContactCard = ({contact,getUID}) => {
 
-    return ( 
+    return (
         <div className="contact-card">
-                <div>
-                    <img src={user} alt="logo" className="user-logo" />
-                </div>
-                <div>
-                    <h3>{contact.name}</h3>
-                    <h3>{contact.email}</h3>
-                    <Link to={`/edit/${contact.id}`}>
-                        <button className="btn edit-btn">Edit</button>
-                    </Link>
-                    <button className="del-btn" onClick={() => getUID(contact.id)}>Delete</button>
-                </div>
+            <div>
+                <img src={user} alt="logo" className="user-logo" />
             </div>
+            <div>
+                <h3>{contact.name}</h3>
+                <h3>{contact.email}</h3>
+                <Link to={`/edit/${contact.id}`}>
+                    <button className="btn edit-btn">Edit</button>
+                </Link>
+                <button className="del-btn" onClick={() => getUID(contact.id)}>Delete</button>
+            </div>
+        </div>
     );
 }
  
