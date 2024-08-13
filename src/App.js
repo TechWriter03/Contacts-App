38160f93740ead5e6ab494ed1e73fe7912eeee8f
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-08-13 20:12:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,72 @@
 import {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
-import Header from './components/Header';
+import EditContact from './components/EditContact';
 
 function App() {
 
-  // const LOCAL_STORAGE_KEY="contacts";
   const API_URL='http://localhost:3500/contacts/';
   const [contacts,setContacts] = useState([]);
   const [userID,setUserID]=useState(0);
+  
+  useEffect(() => {
+    getContacts();
+  },[])
 
-  // POST REQUEST
+  // POST REQUEST (CREATE)
   const addContactHandler = async(contact) => {
-      await fetch(API_URL,{
+      const response = await fetch(API_URL,{
         method:'POST',
         headers:{'Content-Type':'application/json'},
         body:JSON.stringify(contact)
       });
-      setContacts([...contacts, contact]);
+      const data = await response.json();
+      console.log(data + ' is created successfully');
+      getContacts();
   }
 
-  // GET REQUEST
+  // GET REQUEST (READ)
   const getContacts = async() => {
     const response = await fetch(API_URL);
     const data = await response.json();
     setContacts(data);
   }
 
-  useEffect(() => {
-    // const retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    // setContacts(retrievedContacts);
+  // PUT REQUEST (UPDATE)
+  const editContactHandler = async(contact) => {
+    const response = await fetch(API_URL + contact.id,{
+      method:'PUT',
+      headers:{'Content-Type':'application/json'},
+      body:JSON.stringify(contact)
+    });
+    const data = await response.json();
+    console.log(data + ' is updated successfully');
     getContacts();
-  },[])
+  }
 
-  // useEffect(() => {
-    // localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
-  // }, [contacts])
-
-  // DELETE REQUEST
+  // DELETE REQUEST (DELETE)
   const deleteContactHandler = async(id) => {
     setUserID(id);
-    console.log(id);
-    
-    // const newContacts=contacts.filter((contact) => contact.id!==id);
-    // setContacts(newContacts);
-    const response = await fetch(API_URL + id,{
+    const response = await fetch(API_URL + id, {
       method:'DELETE'
     });
     const data = await response.json();
-    console.log(data);
-    setContacts(data);
+    console.log(data + ' is deleted successfully');
+    getContacts();
   }
 
   return (
     <Router>
       <Header/>
       <Routes>
-        <Route eaxct path='/' element={<ContactList contacts={contacts} deleteContactHandler={deleteContactHandler}/>} />
-        <Route path='/add' element={<AddContact addContactHandler={addContactHandler}/>} />
+        <Route exact path='/' element={<ContactList contacts={contacts} deleteContactHandler={deleteContactHandler} />} />
+        <Route path='/add' element={<AddContact addContactHandler={addContactHandler} />} />
+        <Route path='/edit/:id' element={<EditContact contacts={contacts} editContactHandler={editContactHandler} />}/>
       </Routes>
     </Router>
-  );
+  );  
 }
 
 export default App;
